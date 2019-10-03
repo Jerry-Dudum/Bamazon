e0462826ce1cae8f5edf9593ace370f533d8608a
Jerry Dudum <jsdudum@gmail.com>
2019-10-02 18:54:01 -0700

    added values into bamazonDB to populate store with some products

--- bamazon.sql
+++ bamazon.sql
@@ -11,3 +11,15 @@ CREATE TABLE products (
   stock_quanity INT (10) NOT NULL,
   PRIMARY KEY (item_id)
 );
+
+INSERT INTO products (product_name, department_name, price, stock_quanity)
+VALUES ("Monkey D. Luffy Figure", "Figurines", 30, 500),
+("Giorno Giovanna Figure", "Figurines", 25, 999),
+("Keyblade Accessory", "Accessories", 4.99, 1500),
+("Demon Slayer Manga Vol 1", "Manga", 9.99, 2000),
+("Tanjiro's Earrings", "Accessories", 8.25, 400),
+("Fire Force Company 8 Costume", "Cosplay Costumes", 100, 10),
+("A Silent Voice", "Videos", 14.99, 30),
+("Howl's Moving Castle", "Videos", 19.75, 40),
+("Dr. Stone Manga Vol 1", "Manga", 9.99, 350),
+("Demon Slayer Demon Slaying Corps Costume", "Cosplay Costumes", 100, 15)
